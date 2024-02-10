4dfb48919455a81f81e2291bb8c7f42965a8446a
Blizzard_inc <thrashy4inbox@gmail.com>
2024-02-10 21:06:01 +0100

    adding some basic properties of GPseudoMetricSpace
    basically directly copied from the "standard" PseudoMetricSpace in Mathlib
    only small adaptations (think name changes)

--- Mathieu12Simple/GPseudoMetricSpace/Basic.lean
+++ Mathieu12Simple/GPseudoMetricSpace/Basic.lean
@@ -10,6 +10,8 @@ class GDist (α : Type u) (β :Type v) [LinearOrderedAddCommMonoid β] where
   gdist : α → α → β
 
 open Set
+open scoped BigOperators Topology
+
 -- create all bounded sets?
 @[reducible]
 def Bornology.ofGDist (gdist : α → α → β) (gdist_comm : ∀ x y, gdist x y = gdist y x)
@@ -35,10 +37,10 @@ def Bornology.ofGDist (gdist : α → α → β) (gdist_comm : ∀ x y, gdist x 
 
 
 class GPseudoMetricSpace (α : Type u) (β:Type v) [LinearOrderedAddCommMonoid β] extends GDist α β where
-  dist_self : ∀ x : α, gdist x x = 0
-  dist_comm : ∀ x y : α, gdist x y = gdist y x
-  dist_triangle : ∀ x y z : α, gdist x z ≤ gdist x y + gdist y z
-  toBornology : Bornology α := Bornology.ofGDist gdist dist_comm dist_triangle
+  gdist_self : ∀ x : α, gdist x x = 0
+  gdist_comm : ∀ x y : α, gdist x y = gdist y x
+  gdist_triangle : ∀ x y z : α, gdist x z ≤ gdist x y + gdist y z
+  toBornology : Bornology α := Bornology.ofGDist gdist gdist_comm gdist_triangle
   cobounded_sets : (Bornology.cobounded α).sets =
     { s | ∃ C : β, ∀ x ∈ sᶜ, ∀ y ∈ sᶜ, gdist x y ≤ C } := by intros; rfl
 
@@ -60,3 +62,84 @@ theorem GPseudoMetricSpace.ext {m m' : GPseudoMetricSpace α β}
   congr
   · ext : 2
     rw [← Filter.mem_sets, ← Filter.mem_sets, hB, hB']
+
+variable [GPseudoMetricSpace α β]
+
+@[simp]
+theorem gdist_self (x : α) : GDist.gdist x x = (0:β) :=
+  GPseudoMetricSpace.gdist_self x
+
+theorem gdist_comm (x y : α) : GDist.gdist x y = (GDist.gdist y x:β) :=
+  GPseudoMetricSpace.gdist_comm x y
+
+theorem gdist_triangle (x y z : α) : GDist.gdist x z ≤ (GDist.gdist x y + GDist.gdist y z:β) :=
+  GPseudoMetricSpace.gdist_triangle x y z
+
+
+theorem gdist_triangle_left (x y z : α) : GDist.gdist x y ≤ (GDist.gdist z x + GDist.gdist z y:β) := by
+  rw [gdist_comm z]; apply gdist_triangle
+
+
+theorem gdist_triangle_right (x y z : α) : GDist.gdist x y ≤ (GDist.gdist x z + GDist.gdist y z:β) := by
+  rw [gdist_comm y]; apply gdist_triangle
+
+
+theorem gdist_triangle4 (x y z w : α) : GDist.gdist x w ≤ (GDist.gdist x y + GDist.gdist y z + GDist.gdist z w:β) :=
+  calc
+    GDist.gdist x w ≤ GDist.gdist x z + GDist.gdist z w := gdist_triangle x z w
+    _ ≤ (GDist.gdist x y + GDist.gdist y z + GDist.gdist z w:β) := add_le_add_right (gdist_triangle x y z) _
+
+
+theorem gdist_triangle4_left (x₁ y₁ x₂ y₂ : α) :
+    GDist.gdist x₂ y₂ ≤ (GDist.gdist x₁ y₁ + (GDist.gdist x₁ x₂ + GDist.gdist y₁ y₂):β ) := by
+  rw [add_left_comm, gdist_comm x₁, ← add_assoc]
+  apply gdist_triangle4
+
+
+theorem gdist_triangle4_right (x₁ y₁ x₂ y₂ : α) :
+    GDist.gdist x₁ y₁ ≤ (GDist.gdist x₁ x₂ + GDist.gdist y₁ y₂ + GDist.gdist x₂ y₂:β ) := by
+  rw [add_right_comm, gdist_comm y₁]
+  apply gdist_triangle4
+
+
+/-- The triangle (polygon) inequality for sequences of points; `Finset.Ico` version. -/
+theorem gdist_le_Ico_sum_dist (f : ℕ → α) {m n} (h : m ≤ n) :
+    GDist.gdist (f m) (f n) ≤ (∑ i in Finset.Ico m n, GDist.gdist (f i) (f (i + 1)):β ) := by
+  induction n, h using Nat.le_induction with
+  | base => rw [Finset.Ico_self, Finset.sum_empty, gdist_self]
+  | succ n hle ihn =>
+    calc
+      GDist.gdist (f m) (f (n + 1)) ≤ GDist.gdist (f m) (f n) + GDist.gdist (f n) (f (n + 1)) := gdist_triangle _ _ _
+      _ ≤ (∑ i in Finset.Ico m n, _) + _ := add_le_add ihn le_rfl
+      _ = ∑ i in Finset.Ico m (n + 1), _ := by
+      { rw [Nat.Ico_succ_right_eq_insert_Ico hle, Finset.sum_insert, add_comm]; simp }
+
+
+/-- The triangle (polygon) inequality for sequences of points; `Finset.range` version. -/
+theorem gdist_le_range_sum_dist (f : ℕ → α) (n : ℕ) :
+    GDist.gdist (f 0) (f n) ≤ (∑ i in Finset.range n, GDist.gdist (f i) (f (i + 1)) :β ) :=
+  Nat.Ico_zero_eq_range ▸ gdist_le_Ico_sum_dist f (Nat.zero_le n)
+
+
+/-- A version of `gdist_le_Ico_sum_dist` with each intermediate distance replaced
+with an upper estimate. -/
+theorem gdist_le_Ico_sum_of_dist_le {f : ℕ → α} {m n} (hmn : m ≤ n) {d : ℕ → β}
+    (hd : ∀ {k}, m ≤ k → k < n → GDist.gdist (f k) (f (k + 1)) ≤ d k) :
+    GDist.gdist (f m) (f n) ≤ ∑ i in Finset.Ico m n, d i :=
+  le_trans (gdist_le_Ico_sum_dist f hmn) <|
+    Finset.sum_le_sum fun _k hk => hd (Finset.mem_Ico.1 hk).1 (Finset.mem_Ico.1 hk).2
+
+
+/-- A version of `gdist_le_range_sum_dist` with each intermediate distance replaced
+with an upper estimate. -/
+theorem gdist_le_range_sum_of_dist_le {f : ℕ → α} (n : ℕ) {d : ℕ → β}
+    (hd : ∀ {k}, k < n → GDist.gdist (f k) (f (k + 1)) ≤ d k) :
+    GDist.gdist (f 0) (f n) ≤ ∑ i in Finset.range n, d i :=
+  Nat.Ico_zero_eq_range ▸ gdist_le_Ico_sum_of_dist_le (zero_le n) fun _ => hd
+
+
+theorem swap_gdist : Function.swap GDist.gdist = (GDist.gdist:α → α → β) := by funext x y; exact gdist_comm _ _
+
+
+theorem gdist_nonneg {x y : α} : 0 ≤ (GDist.gdist x y:β) :=
+  gdist_nonneg' GDist.gdist gdist_self gdist_comm gdist_triangle
